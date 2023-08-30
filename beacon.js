d260683b7026da7b0c6fb771774d63d15b53949c
savetlm <143538045+savetlm@users.noreply.github.com>
2023-08-30 09:34:16 -0600

    Add files via upload

---
+++ beacon.js
@@ -0,0 +1,17 @@
+//var dropdownMenu;
+function dropdownMenu(){
+   var x = document.getElementById("menuClick");
+            if(x.className === "navbar"){
+                x.className += " responsive";
+            }
+            else{
+                x.className = "navbar"; 
+            }
+            
+        } 
+
+        
+            
+               
+            
+        
